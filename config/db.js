8742721ad58bd92c47dbd38d10f30410782200dc
Darío Antonio Gutiérrez Álvarez <darioantoniogutierrezalvarez@MacBook-Air.local>
2024-10-29 11:50:49 -0600

    [FIX]

--- config/db.js
+++ config/db.js
@@ -1,16 +1,17 @@
+// config/db.js
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'coepes'
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to the database:', err);
-    return;
+	console.error('Error connecting to the database:', err);
+	return;
   }
   console.log('Connected to the MySQL database.');
 });
